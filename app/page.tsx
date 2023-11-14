905846d5baa979438f610a83d4be990a7fc56119
Tuan Dau <tukauu@gmail.com>
2023-11-14 17:03:02 -0500

    add: nextjs/ts/tailwind boilerplate

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col">
+      <div>navbar goes here</div>
+      <div>This would be my first line of text</div>
+    </main>
+  );
+}
