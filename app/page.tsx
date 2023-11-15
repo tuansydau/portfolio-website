a74301b98ef5d11d0e64182f4b99eff778eb8404
tuansydau <tukauu@gmail.com>
2023-11-14 20:37:09 -0500

    added: navbar, intro outline

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,37 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col">
-      <div>navbar goes here</div>
-      <div>This would be my first line of text</div>
-    </main>
+    <div className="flex min-h-screen flex-col">
+      <div className="flex w-full justify-end">
+        <div className="flex w-[24rem] h-24 items-center justify-between mr-16">
+          <div className="flex px-4 h-12 justify-center items-center text-lg font-semibold border-b-4 text-[#abb589] border-[#abb589]">Work</div>
+          <div className="flex h-12 justify-center items-center text-lg font-semibold text-[#abb589]">About</div>
+          <div className="flex h-12 justify-center items-center text-lg font-semibold text-[#abb589]">Resume</div>
+        </div>
+      </div>
+      
+      {/* intro section */}
+      <div className="flex flex-row items-center justify-center px-64 mt-24">
+        <div className="flex text-5xl">this empty box will be a photo or something</div>
+        <div>
+          <div className="flex flex-row text-4xl font-light font-['Helvetica Neue']">
+            Hey, I'm Tuan, a new-grad full-stack developer with a love of usable products. I make web apps that users love.
+          </div>
+          <div className="flex h-12 w-fit rounded-xl mt-4 px-4 border-2 text-[#abb589] border-[#abb589] font-bold items-center justify-center">
+            Hire me
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col px-64 mt-16">
+        <div className="text-3xl font-bold mt-16 text-[#abb589]">Work</div>
+        <div className="">item 1</div>
+        <div>item 2</div>
+        <div>item 3</div>
+        <div>item 4</div>
+        <div>item 5</div>
+        <div>item 6</div>
+      </div>
+    </div>
   );
 }
