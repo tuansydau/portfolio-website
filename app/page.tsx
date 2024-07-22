667290bf9c22d1ddae0137f4fd5392feb59efaa8
tuansydau <tukauu@gmail.com>
2024-07-22 01:12:12 -0400

    revives: version 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,24 @@
 'use client';
+
 import Navbar from '@/components/Navbar';
-import useMousePosition from '@/utils/useMousePosition';
+import Footer from '@/components/Footer';
+import Modal from '@/components/Modal';
 import { motion } from 'framer-motion';
 import { useState } from 'react';
+import IntroSection from '@/components/App/IntroSection';
+import useMousePosition from '@/utils/useMousePosition';
 import ProjectCard from '@/components/App/ProjectCard';
-import Footer from '@/components/Footer';
 
 interface MousePosition {
   x: number;
   y: number;
 }
+
 export default function Home() {
+  const [modalOpen, setModalOpen] = useState<boolean>(false);
   const { x, y } = useMousePosition();
   const [isHover, setIsHover] = useState<boolean>(false);
-  var size = isHover ? 250 : 50;
+  var size = isHover ? 300 : 50;
   var color = 'white';
   var mixBlendMode = 'difference';
 
@@ -21,9 +26,9 @@ export default function Home() {
   const textLeave = () => setIsHover(false);
 
   return (
-    <div className="flex pl-4 lg:pl-0 min-h-screen flex-col items-center justify-between overflow-hidden">
-      {/* page container */}
-      {/* Navbar */}
+    <div className="flex min-h-screen flex-col items-center">
+      <Navbar highlightedButton={'home'} />
+      <div className="cursor-outline" data-cursor-outline></div>
       <motion.div
         className="cursor z-50"
         style={{
@@ -42,208 +47,45 @@ export default function Home() {
         }}
         transition={{ type: 'spring', stiffness: 300 }}
       />
-      <div className="fixed w-screen bg-white z-10">
-        <Navbar highlightedButton={'home'} />
-      </div>
-      <div className="flex w-full min-h-screen max-w-[1360px]">
-        {/* Body container */}
-        <div className="w-full">
-          {/* Hero section */}
-          <div className="flex flex-col lg:flex-row w-full justify-between">
-            <div
-              className="mt-32 lg:w-7/12"
-              style={{
-                fontFamily: 'comico',
-                fontSize: '100px',
-                lineHeight: '100%',
-                letterSpacing: '-0.25rem',
-                fontWeight: 900
-              }}
-              onMouseEnter={textEnter}
-              onMouseLeave={textLeave}
-            >
-              <span
-              // className="-ml-6"
-              >
-                TUAN DAU
-              </span>
-              <div className="flex h-[120px] items-center">
-                {/* <div className="bg-black w-[400px] h-[90px] mr-2" /> */}
-                IS NOW A
-              </div>
-              DEVELOPER
-            </div>
-            <div
-              className="w-1/3 mt-32 invisible lg:visible rotate-6"
-              style={{
-                fontFamily: 'inter',
-                fontSize: '80px',
-                lineHeight: '100%',
-                fontWeight: 400,
-                zIndex: 0,
-                paddingTop: '10rem'
-              }}
-              onMouseEnter={textEnter}
-              onMouseLeave={textLeave}
-            >
-              &quot;I am truly ecstatic&quot; <br />- Tuan Dau
-            </div>
-          </div>
-          <div
-            className="w-full text-right mt-32 text-[#414141]"
-            style={{
-              fontFamily: 'zodiak',
-              fontSize: '60px',
-              lineHeight: '100%',
-              letterSpacing: '-0.25rem',
-              fontWeight: 300
-            }}
-            onMouseEnter={textEnter}
-            onMouseLeave={textLeave}
-          >
-            &quot;About time&quot;, says Friend
-          </div>
-
-          {/* About me */}
-          <div className="flex w-full justify-between min-h-screen items-center mt-32 border-black">
-            <div className="w-full lg:flex">
-              <div
-                className="w-4/12"
-                style={{
-                  fontFamily: 'zodiak, sans-serif',
-                  fontSize: '20px',
-                  letterSpacing: '0rem',
-                  fontWeight: 900
-                }}
-              >
-                (Well, I mean)
-              </div>
-              <div
-                className="w-8/12"
-                style={{
-                  fontFamily: 'satoshi, sans-serif',
-                  fontSize: '35px',
-                  lineHeight: '110%',
-                  letterSpacing: '-0.05rem',
-                  fontWeight: 300
-                }}
-              >
-                Tuan was always a developer, but up until recently he was
-                unemployed, and still finding his niche. Now that Tuan has a
-                job, he can finally say that he is a full stack developer.
-                <br />
-                <br />
-                Tuan is currently a Full Stack Developer, and occasional UI/UX
-                designer at{' '}
-                <a
-                  href="https://www.arimadata.com"
-                  className="font-medium text-green-500"
-                >
-                  Arima Data
-                </a>
-                .
-                <br />
-                <br />
-                <div>Tuan has done the following before:</div>
-                <ul
-                  className="mt-3"
-                  style={{
-                    fontFamily: 'satoshi',
-                    fontSize: '25px',
-                    lineHeight: '200%',
-                    letterSpacing: '-0.05rem',
-                    fontWeight: 300,
-                    listStyleType: 'disc',
-                    listStylePosition: 'inside'
-                  }}
-                >
-                  <li>
-                    Back End - Python (Flask, FastAPI), MongoDB, PostgreSQL,
-                    Ethereum/Solidity
-                  </li>
-                  <li>
-                    Front End - Typescript/Javascript/React, Valtio, HTML/CSS,
-                    Framer Motion
-                  </li>
-                  <li>
-                    DevOps - Azure/AWS, DigitalOcean, Jenkins, Github Actions,
-                    Docker
-                  </li>
-                  <li>UI/UX Design - Figma, User Interviews</li>
-                  <br />
-                </ul>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col w-full max-w-[1360px]">
-            <div className="flex w-full space-x-4">
-              <ProjectCard
-                category="Project"
-                title="OpenGL Shaders"
-                description="A collection of OpenGL fragment shaders that I made while
+      <Modal isModalOpen={modalOpen} modalSetter={setModalOpen} />
+      <IntroSection
+        textEnterFunction={textEnter}
+        textExitFunction={textLeave}
+        modalFunction={setModalOpen}
+      />
+      <div className="flex w-4/5 space-x-4">
+        <ProjectCard
+          category="Project"
+          title="OpenGL Shaders"
+          description="A collection of OpenGL fragment shaders that I made while
                   learning graphics processing."
-                imagePath="/Animation-3.gif"
-                linkTo="https://github.com/tuansydau/shader-work"
-                targetBlank
-              />
-              {/* <ProjectCard
-            category="Internship"
-            title="Big Whale Labs Website"
-            description="A website redesign that I helped program while interning at Big Whale Labs."
-            imagePath="/bwl_homepage.png"
-            linkTo="/bwl-homepage"
-            filter="grayscale(100%)"
-          /> */}
-              <ProjectCard
-                category="Project"
-                title="Tab2XML"
-                description="A program that converts guitar tablature into a playable MusicXML file."
-                imagePath="/tab2xml.png"
-                linkTo="https://github.com/tuansydau/TAB2XML"
-                filter="grayscale(100%)"
-                lowBaseOpacity
-                targetBlank
-              />
-            </div>
-            <br />
-            <div className="flex w-full space-x-4">
-              <ProjectCard
-                category="Project"
-                title="3D Globe Visualizing Global Transactions"
-                description="A 3D Globe that visualizes transactions made on Float's card system, submitted for DeCode 2021."
-                imagePath="/three-globe.png"
-                linkTo="https://github.com/trentstauff/decode-2021/tree/main/frontend"
-                targetBlank
-                filter="grayscale(100%)"
-              />
-              <ProjectCard
-                category="Project"
-                title="NiftyArt"
-                description="A simple marketplace to buy and sell digital artwork, built on Ethereum."
-                imagePath="/niftyArt.jpg"
-                linkTo="https://github.com/tuansydau/TAB2XML"
-                filter="grayscale(100%)"
-                targetBlank
-                lowBaseOpacity
-              />
-            </div>
-          </div>
-          <div className="mt-24"></div>
-          <Footer />
-          {/* <div
-            className="flex flex-col w-full items-center min-h-screen"
-            style={{
-              fontFamily: 'satoshi',
-              fontSize: '60px',
-              lineHeight: '110%',
-              letterSpacing: '0rem',
-              fontWeight: 700,
-              listStyleType: 'circle'
-            }}
-          ></div> */}
-          {/* <div className="min-h-screen"></div> */}
-        </div>
+          imagePath="/Animation-3.gif"
+          linkTo="/shaders"
+        />
+        <ProjectCard
+          category="Internship"
+          title="Big Whale Labs Website"
+          description="A website redesign that I helped program while interning at Big Whale Labs."
+          imagePath="/bwl_homepage.png"
+          linkTo="/bwl-homepage"
+          filter="grayscale(100%)"
+        />
       </div>
+      <br />
+      <div className="flex w-4/5">
+        <ProjectCard
+          category="Project"
+          title="3D Globe Visualizing Global Transactions"
+          description="A remake of a previous hackathon submission completed with Float's team at Decode 2021. Inspired by the GitHub globe."
+          imagePath="/three-globe.png"
+          linkTo="https://github.com/trentstauff/decode-2021/tree/main/frontend"
+          targetBlank
+          filter="grayscale(100%)"
+        />
+      </div>
+      <br />
+      <br />
+      <Footer />
     </div>
   );
 }
--- components/App/IntroSection.tsx
+++ components/App/IntroSection.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-
 interface IntroSectionProps {
   textEnterFunction?: () => void;
   textExitFunction?: () => void;
@@ -13,50 +11,19 @@ export default function IntroSection({
 }: IntroSectionProps) {
   return (
     <div
-      className="flex flex-col w-full h-screen items-center justify-center"
+      className="flex flex-col w-full items-center h-[800px] justify-center "
       onMouseEnter={textEnterFunction}
       onMouseLeave={textExitFunction}
     >
-      {/* <div
-        className="px-24 w-full"
-        style={{
-          fontFamily: 'satoshi',
-          fontSize: '2.5vw',
-          fontWeight: '900',
-          paddingRight: '40vw'
-        }}
-      >
-        Hello!{' '}
-        <span style={{ fontWeight: '100' }}>
-          I&apos;m Tuan, a fullstack engineer from Toronto! I&apos;m currently
-          working at{' '}
-          <span className="text-[#2fd063]" style={{ fontWeight: '600' }}>
-            Arima Data
-          </span>{' '}
-          as a <span style={{ fontWeight: '500' }}>Frontend Engineer</span> and{' '}
-          <span style={{ fontWeight: '500' }}>Designer</span>.
-          <div
-            className="flex bg-white font-light text-black text-[1.75vw] font-thin hover:border-white hover:bg-black hover:text-white hover:cursor-pointer border-b-3 border-r-2 h-[10vh] mt-[5vh] w-[9vw] px-2 border border-black items-center justify-center"
-            onClick={() => modalFunction(true)}
-            style={{
-              fontFamily: 'satoshi, serif'
-            }}
-          >
-            Let&apos;s talk!
-          </div>
-        </span>
-      </div> */}
-
-      {/* Short Magazine Blurb */}
-      <div className="flex w-full max-w-[2000px] flex-col lg:flex-row md:space-x-4 px-24">
-        <div className="flex flex-col items-center -ml-16">
+      <div className="flex flex-col lg:flex-row items-center bg-white justify-center md:space-x-4 -mt-16 px-4">
+        <div className="flex flex-col max-w-[1200px] justify-center items-center -ml-16">
           <div className="flex">
-            <div className="bg-black w-2 h-[360px] mt-8 ml-16" />
+            <div className="bg-black w-4 h-[360px] mt-8 -mr-8" />
             <div className="flex flex-col text-4xl rounded-md px-32 border-black">
               <div className="border-b-3 w-fit border-black">
                 <p
                   style={{
-                    fontFamily: 'satoshi, serif',
+                    fontFamily: 'Zodiak, serif',
                     fontSize: '4rem',
                     lineHeight: '8rem',
                     marginLeft: '-1rem'
@@ -67,34 +34,33 @@ export default function IntroSection({
               </div>
               <br />
               <p
-                className="w-full max-w-[800px]"
                 style={{
-                  fontFamily: 'satoshi, serif',
+                  fontFamily: 'Zodiak, serif',
                   fontSize: '1.25rem',
                   lineHeight: '3rem'
                 }}
               >
-                Full-stack developer, designer, and occasional devops engineer (out of necessity, mainly). I&apos;ve
-                interned at a number of different startups and companies, creating tools
+                Full-stack developer, designer, and DevOps engineer. I&apos;ve
+                worked at industry leaders like Tesla and CertiK, creating tools
                 and pipelines to automate work that people hate doing. Also an
                 avid tea, music, and Runescape enjoyer.
               </p>
               <br />
               <p
                 style={{
-                  fontFamily: 'satoshi, serif',
+                  fontFamily: 'Zodiak, serif',
                   fontSize: '1.25rem',
                   fontWeight: 600
                 }}
               >
-                Currently: Front End Software Engineer at <a href="https://www.arimadata.com" target='_blank' className='text-[#1bcc04]'>Arima Data</a>, blasting Knock2, and redesigning this website
+                Currently: Blasting Knock2 and redesigning this website
               </p>
               <br />
               <div
-                className="flex bg-white text-black text-xl hover:border-white hover:bg-black hover:text-white hover:cursor-pointer border-b-3 border-r-2 h-12 w-fit px-4 border border-black items-center justify-center"
+                className="flex bg-white text-black text-xl hover:border-white hover:bg-black hover:text-white hover:cursor-pointer border-b-3 border-r-2 h-12 w-fit rounded-full px-4 border border-black items-center justify-center"
                 onClick={() => modalFunction(true)}
                 style={{
-                  fontFamily: 'satoshi, serif'
+                  fontFamily: 'Zodiak, serif'
                 }}
               >
                 Let&apos;s talk!
@@ -103,65 +69,6 @@ export default function IntroSection({
           </div>
         </div>
       </div>
-
-      {/* Designers/writers theme */}
-      {/* <div className="w-full h-full max-w-[2000px]">
-        <div
-          className="w-full h-full"
-          style={{ fontFamily: 'satoshi' }}
-          onMouseEnter={textEnterFunction}
-          onMouseLeave={textExitFunction}
-        >
-          <div className="w-full h-full flex flex-col items-center justify-between">
-            <div
-              className="-mt-24"
-              style={{
-                fontSize: '14rem',
-                lineHeight: '14rem',
-                fontWeight: 600
-              }}
-            >
-              programmer
-            </div>
-            <div className="flex flex-col w-full items-center">
-              <div
-                className="flex justify-center items-center w-1/2"
-                style={{ fontSize: '2rem' }}
-              >
-                <b style={{ fontWeight: 900 }}>tuan dau: </b>
-                <div className="pl-2">portfolio website</div>
-              </div>
-              <div className="w-1/2 text-center" style={{ fontSize: '.95rem' }}>
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industrys standard dummy text
-                ever since the 1500s, when an unknown printer took a galley of
-                type and scrambled it to make a type specimen book. It has
-                survived not only five centuries, but also the leap into
-                electronic typesetting, remaining essentially unchanged. It was
-                popularised in the 1960s with the release of Letraset sheets
-                containing Lorem Ipsum passages, and more recently with desktop
-                publishing software like Aldus PageMaker including versions of
-                Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing
-                and typesetting industry. Lorem Ipsum has been the industrys
-                standard dummy text ever since the 1500s, when an unknown
-                printer took a galley of type and scrambled it to make a type
-                specimen book. It has survived not only five centuries, but also
-                the leap into electronic typesetting, remaining essentially
-                unchanged. It was popularised in the 1960s with the release of
-                Letraset sheets containing Lorem Ipsum passages, and more
-                recently with desktop publishing software like Aldus PageMaker
-                including versions of Lorem Ipsum.
-              </div>
-            </div>
-            <div
-              className="flex justify-center w-full"
-              style={{ fontSize: '7rem', lineHeight: '7rem', fontWeight: 900 }}
-            >
-              <div>(sometimes designer)</div>
-            </div>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 }
